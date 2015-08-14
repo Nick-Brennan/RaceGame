4e4c30ad7bad0daf6f71db4f610cc9cec1832027
Nick Brennan <nick_p_b@yahoo.com>
2015-08-14 16:49:27 -0700

    Added all of the race game files to the repo

---
+++ code.js
@@ -0,0 +1,55 @@
+$(document).ready(function(){
+    var gCell = 0;
+    var bCell = 0;
+    
+    var greenCar = '<img src="greenCar.png" alt="X">';
+    var greyCar = '<img src="greyCar.png" alt="X">';
+     
+    
+    $(window).keypress(function(e) {
+           
+        var key = e.which;
+           //do stuff with "key" here...
+            //$('#message').text(key);
+        if(key === 97){
+            //$('#message').text('"A" key pressed');
+            $('#player1-track div').eq(gCell).text("");
+            $('#player1-track div').eq(gCell + 1).text("X");
+            checkForWinner();
+            gCell++;
+        }
+        else if(key === 108){
+            //$('#message').text('"L" key pressed');
+            $('#player2-track div').eq(bCell).text("");
+            $('#player2-track div').eq(bCell + 1).text("X");
+            checkForWinner();
+            bCell++;
+        }
+        if(gCell > 9 || bCell > 9){
+        return;
+    }
+        
+            
+   });
+    
+});
+
+function checkForWinner(){
+    if(($('#g10').text() === 'X') && ($('#b10').text() === "")){
+        $('#message').text('Grey Racer Wins!');
+        window.location = 'greyWins.html'
+        return true;
+    } else if(($('#b10').text() === 'X') && ($('#g10').text() === "")){
+        $('#message').text('Blue Racer Wins!');
+        window.location = 'greenWins.html'
+        return true;
+    } else {
+        return false;
+    }
+}
+//function buildArray(array,$obj){
+//    for(i = 0; i <10; i++){
+//        array.push($obj.eq(i));
+//    }
+//    return array;
+//}
