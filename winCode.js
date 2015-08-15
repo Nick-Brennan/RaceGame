021a3b321c1d8a29605e6130c9d1c7af578ffa94
Nick Brennan <nick_p_b@yahoo.com>
2015-08-15 15:16:11 -0700

    Removed unused IDs from index.html , added stytles to 'Play Again' buttons on hover

--- winCode.js
+++ winCode.js
@@ -2,4 +2,8 @@ $(document).ready(function(){
     $('div').on("click", function(){
         window.location = "index.html";
     });
+    
+    $('div').hover(function(){
+        $('div').toggleClass('hover');
+    });
 });
